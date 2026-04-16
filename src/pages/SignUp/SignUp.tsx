d9aeb6691e277eae5dbf1f2b577e360e75869d72
AtheoJester23 <atheojester@gmail.com>
2026-04-17 02:17:53 +0800

    feat(signup page): add error handling

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,21 +1,56 @@
 import { Eye, EyeClosed } from "lucide-react";
 import { useState, type SubmitEvent } from "react";
 
+type possibleErrors = {
+    email: boolean,
+    password: boolean,
+    confirmPassword: boolean,
+}
+
 const SignUp = () => {
     const [hidden, setHidden] = useState(true);
     const [loading, setLoading] = useState(false);
+    const [errors, setErrors] = useState<possibleErrors>({
+        email: false,
+        password: false,
+        confirmPassword: false,
+    });
 
     const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
         e.preventDefault();
         setLoading(true)
-        try {
-            const formData = new FormData(e.currentTarget);
-            const firstName = formData.get("firstName") as string;
-            const lastName = formData.get("lastName") as string;
-            const email = formData.get("email") as string;
-            const storeName = formData.get("storeName") as string;
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get("email") as string;
+        const password = formData.get("password") as string;
+        const confirmPassword = formData.get("confirmPassword") as string;
+        
+        const errs = {...errors};
 
-            console.log(firstName, lastName, email, storeName);
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(!email || !emailRegex.test(email)) {
+            errs.email = true;
+        }
+
+        if(!password || password.length < 6) {
+            errs.password = true;
+        }
+
+        if(password !== confirmPassword) {
+            errs.confirmPassword = true;
+        }
+
+        setErrors(errs);
+
+        if(Object.values(errs).some(Boolean)) {
+            setLoading(false);
+            return;
+        }
+
+
+        try {
+
+            console.log(email, password, confirmPassword);
         } catch (error) {
             console.error((error as Error).message)
         }finally{
@@ -27,12 +62,11 @@ const SignUp = () => {
     <main className="simpleMains">
       <form className="simpleForms shadow" onSubmit={handleSubmit}>
         <h1 className="text-4xl font-bold">Sign-up</h1>
-        {/* <div className="flex gap-3">
-            <input type="text" name="firstName" placeholder="First Name"/>
-            <input type="text" name="lastName" placeholder="Last Name"/>
-        </div> */}
-        <input type="email" name="email" placeholder="Email" autoComplete="off"/>
-        {/* <input type="text" name="storeName" placeholder="Store Name" autoComplete="off"/> */}
+        <div className="flex flex-col">
+            <input type="email" name="email" placeholder="Email" autoComplete="off" onChange={() => setErrors(prev => ({...prev, email: false}))}/>
+            {errors.email && <small className="text-red-500">Invalid email address*</small>}
+        </div>
+        
         <div className="relative">
             <button type="button" onClick={() => setHidden(!hidden)} className="absolute right-3 top-3 hover:cursor-pointer active:cursor-default">
                 {hidden
@@ -40,9 +74,13 @@ const SignUp = () => {
                     : <Eye/>
                 }
             </button>
-            <input type={hidden ? "password" : "text"} name="password" placeholder="Password" autoComplete="off" className="pe-[40px]!"/>
+            <input type={hidden ? "password" : "text"} name="password" placeholder="Password" autoComplete="off" className="pe-[40px]!" onChange={() => setErrors(prev => ({...prev, password: false, confirmPassword: false}))}/>
         </div>
-        <input type={hidden ? "password" : "text"} name="confirmPassword" placeholder="Confirm Password" autoComplete="off"/>
+        {errors.password && <small className="text-red-500">Password must be at least 7 characters*</small>}
+        <div className="flex flex-col">
+            <input type={hidden ? "password" : "text"} name="confirmPassword" placeholder="Confirm Password" autoComplete="off" onChange={() => setErrors(prev => ({...prev, password: false, confirmPassword: false}))}/>
+            {errors.confirmPassword && <small className="text-red-500">Passwords do not match*</small>}
+        </div>
         {!loading
             ? <button className="submitBtn">Submit</button>
             : <div className="loadingBtn">Loading</div>
