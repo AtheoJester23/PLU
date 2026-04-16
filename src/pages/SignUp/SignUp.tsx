7daf09c0f18a723ac9213a849e28b45d8b557216
AtheoJester23 <atheojester@gmail.com>
2026-04-17 02:04:15 +0800

    feat(signup page): eye button

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,6 +1,8 @@
+import { Eye, EyeClosed } from "lucide-react";
 import { useState, type SubmitEvent } from "react";
 
 const SignUp = () => {
+    const [hidden, setHidden] = useState(true);
     const [loading, setLoading] = useState(false);
 
     const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
@@ -10,10 +12,10 @@ const SignUp = () => {
             const formData = new FormData(e.currentTarget);
             const firstName = formData.get("firstName") as string;
             const lastName = formData.get("lastName") as string;
+            const email = formData.get("email") as string;
             const storeName = formData.get("storeName") as string;
-            const bio = formData.get('bio') as string;
 
-            console.log(firstName, lastName, storeName, bio);
+            console.log(firstName, lastName, email, storeName);
         } catch (error) {
             console.error((error as Error).message)
         }finally{
@@ -25,13 +27,26 @@ const SignUp = () => {
     <main className="simpleMains">
       <form className="simpleForms shadow" onSubmit={handleSubmit}>
         <h1 className="text-4xl font-bold">Sign-up</h1>
-        <div className="flex gap-3">
+        {/* <div className="flex gap-3">
             <input type="text" name="firstName" placeholder="First Name"/>
             <input type="text" name="lastName" placeholder="Last Name"/>
+        </div> */}
+        <input type="email" name="email" placeholder="Email" autoComplete="off"/>
+        {/* <input type="text" name="storeName" placeholder="Store Name" autoComplete="off"/> */}
+        <div className="relative">
+            <button type="button" onClick={() => setHidden(!hidden)} className="absolute right-3 top-3 hover:cursor-pointer active:cursor-default">
+                {hidden
+                    ? <EyeClosed/>
+                    : <Eye/>
+                }
+            </button>
+            <input type={hidden ? "password" : "text"} name="password" placeholder="Password" autoComplete="off" className="pe-[40px]!"/>
         </div>
-        <input type="text" name="storeName" placeholder="Store Name" autoComplete="off"/>
-        <textarea name="bio" placeholder="Bio" autoComplete="off"/>
-        <button className="">Submit</button>
+        <input type={hidden ? "password" : "text"} name="confirmPassword" placeholder="Confirm Password" autoComplete="off"/>
+        {!loading
+            ? <button className="submitBtn">Submit</button>
+            : <div className="loadingBtn">Loading</div>
+        }
       </form>
     </main>
   )
