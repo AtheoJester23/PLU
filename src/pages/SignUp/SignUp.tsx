0c0e9d43802e89bab49ce2616a3f0b9bc7e33735
AtheoJester23 <atheojester@gmail.com>
2026-04-17 23:35:10 +0800

    feat(signup page): add error handler

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -2,10 +2,12 @@ import { Eye, EyeClosed } from "lucide-react";
 import { useState, type SubmitEvent } from "react";
 import { motion } from "framer-motion";
 import supabase from "../../config/supabaseClient";
+import { useNavigate } from "react-router-dom";
 
 type possibleErrors = {
     firstName: boolean,
     lastName: boolean,
+    store_name: boolean,
     email: boolean,
     password: boolean,
     confirmPassword: boolean,
@@ -17,10 +19,13 @@ const SignUp = () => {
     const [errors, setErrors] = useState<possibleErrors>({
         firstName: false,
         lastName: false,
+        store_name: false,
         email: false,
         password: false,
         confirmPassword: false,
     });
+
+    const navigate = useNavigate();
 
     const handleSubmit = async (e: SubmitEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -37,6 +42,20 @@ const SignUp = () => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         
         console.log(first_name, last_name, store_name);
+        
+        if(!first_name || first_name.replace(/[ ]/g, '') == ""){
+            errs.firstName = true;
+        }
+
+        if(!last_name || last_name.replace(/[ ]/g, '') == ""){
+            errs.lastName = true;
+        }
+
+        if(!store_name || store_name.replace(/[ ]/g, "") == ""){
+            errs.store_name = true;
+        }
+        
+        
         if(!email || !emailRegex.test(email)) {
             errs.email = true;
         }
@@ -82,6 +101,8 @@ const SignUp = () => {
             }
 
             console.log("Account created successfully.");
+
+            navigate('/');
         } catch (error) {
             console.error((error as Error).message)
         }finally{
@@ -101,8 +122,14 @@ const SignUp = () => {
         <h1 className="text-4xl font-bold">Sign-up</h1>
         
         <div className="flex gap-2">
-            <input type="text" name="firstName" placeholder="firstName"/>
-            <input type="text" name="lastName" placeholder="lastName"/>
+            <div className="flex flex-col">
+                <input onChange={() => setErrors(prev => ({...prev, firstName: false}))} type="text" name="firstName" placeholder="First Name"/>
+                {errors.firstName && <small className="text-red-500">First Name is required*</small>}
+            </div>
+            <div className="flex flex-col">
+                <input onChange={() => setErrors(prev => ({...prev, lastName: false}))} type="text" name="lastName" placeholder="Last Name"/>
+                {errors.lastName && <small className="text-red-500">Last Name is required*</small>}
+            </div>
         </div>
         
         <div className="flex flex-col">
@@ -110,8 +137,9 @@ const SignUp = () => {
             {errors.email && <small className="text-red-500">Invalid email address*</small>}
         </div>
         
-        <div>
-            <input className="w-full" name="storeName" type="text" placeholder="Store Name"/>
+        <div className="flex flex-col">
+            <input onChange={() => setErrors(prev => ({...prev, store_name: false}))} className="w-full" name="storeName" type="text" placeholder="Store Name"/>
+            {errors.store_name && <small className="text-red-500">Store name is required*</small>}
         </div>
         
         <div className="relative">
@@ -134,6 +162,10 @@ const SignUp = () => {
             ? <button className="submitBtn">Submit</button>
             : <div className="loadingBtn">Loading</div>
         }
+
+        <div className="flex justify-center items-center">
+            <p>Already have an account? <span onClick={() => navigate('/')} className="text-blue-500 hover:cursor-pointer font-bold underline">Login</span></p>
+        </div>
       </motion.form>
     </main>
   )
