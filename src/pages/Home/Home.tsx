30c8c59326ac8daad9177b17e2edf6f932fe58ed
AtheoJester23 <atheojester@gmail.com>
2026-04-28 20:51:46 +0800

    feat: search function

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import supabase from "../../config/supabaseClient";
 import { ChevronDown, Funnel, Plus, TriangleAlert } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, type SubmitEvent } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "../../state/store";
 import { ClipLoader } from "react-spinners";
@@ -72,6 +72,18 @@ const Home = () => {
       }
     }
 
+    const linearSearch = (e: SubmitEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const formData = new FormData(e.currentTarget);
+
+      const target = formData.get("searchProds") as string;
+
+
+      const filtered = products.filter(item => item.name.toLocaleLowerCase().includes(target.toLocaleLowerCase()));
+      setProducts(filtered);
+    }
+
     const handleFilterProducts = (category: string) => {
       setOpen(false);
       console.log(prods);
@@ -89,7 +101,13 @@ const Home = () => {
       <h1 className="text-3xl font-bold p-0 text-nav text-center">Products</h1>
       
       <div className="flex justify-between fixed py-2 top-16 left-0 right-0 bg-navBtn z-100 px-5">
-        <input type="text" placeholder="Search products..." className="bg-white px-5 rounded outline-none"/>
+        <form onSubmit={(e) => linearSearch(e)}>
+          <input onChange={(e) => {
+            if(e.currentTarget.value == ""){
+              setProducts(prods);
+            }
+          }} type="text" name="searchProds" placeholder="Search products..." className="h-full bg-white px-5 rounded outline-none"/>
+        </form>
         <div className="flex gap-3 justify-center items-center">
           <small>sort by: </small>
           <div className="relative">
@@ -116,21 +134,30 @@ const Home = () => {
                 <p className="font-bold text-4xl text-nav">No products yet...</p>
             </Link>
           ):(
-            <ul className="grid max-sm:grid-cols-2 grid-cols-5 gap-5 mt-5 pb-5 w-full">
-                {products.map(item => (
-                    <li key={item.id} className="bg-white flex flex-col items-center p-5 shadow-lg rounded gap-3 hover:translate-y-2 hover:shadow-none duration-350 cursor-pointer active:cursor-default">
-                        <Link to={`/edit/${item.id}`}>
-                          <div className="max-sm:h-[110px] h-[230px] w-full overflow-hidden flex justify-center items-center">
-                              <img src={item.picture} alt={item.name} />
-                          </div>
-                          <div>
-                              <h2>{item.name}</h2>
-                              <p>₱{item.price.toFixed(2)}</p>
-                          </div>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <>
+              {products.length < 1 ? (
+                <Link to={"/create"} className={`cursor-pointer flex flex-col mt-5 w-full h-[100%] flex justify-center items-center border-3 border-nav border-dashed rounded`}>
+                  <Plus aria-hidden="false" className="h-30 text-nav" size={100}/>
+                  <p className="font-bold text-4xl text-nav">That product does not exist yet...</p>
+              </Link>
+              ):(
+                <ul className="grid max-sm:grid-cols-2 grid-cols-5 gap-5 mt-5 pb-5 w-full">
+                    {products.map(item => (
+                        <li key={item.id} className="bg-white flex flex-col items-center p-5 shadow-lg rounded gap-3 hover:translate-y-2 hover:shadow-none duration-350 cursor-pointer active:cursor-default">
+                            <Link to={`/edit/${item.id}`}>
+                              <div className="max-sm:h-[110px] h-[230px] w-full overflow-hidden flex justify-center items-center">
+                                  <img src={item.picture} alt={item.name} />
+                              </div>
+                              <div>
+                                  <h2>{item.name}</h2>
+                                  <p>₱{item.price.toFixed(2)}</p>
+                              </div>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+              )}
+            </>
           )}
         </>
       )}
