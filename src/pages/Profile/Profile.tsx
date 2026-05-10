9a7b2b0e294ebb617bf59357edfb3b4b597b24d0
AtheoJester23 <atheojester@gmail.com>
2026-05-11 01:34:08 +0800

    feat(profile): add delete profile function

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -5,12 +5,21 @@ import { useState, type SubmitEvent } from 'react';
 import supabase from '../../config/supabaseClient';
 import {ToastContainer, toast} from 'react-toastify';
 import { setStoreName, setUser } from '../../state/auth/authSlice';
+import { motion } from 'framer-motion';
+import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react';
+import { TriangleAlert } from 'lucide-react';
 
 const Profile = () => {
   const user = useSelector((state: RootState) => state.auth.user);
   const [edit, setEdit] = useState(false);
   const theme = useSelector((state: RootState) => state.ui.theme);
   const dispatch = useDispatch<AppDispatch>();
+  
+  const [delAcc, setDelAcc] = useState(false);
+
+  const setConfirmDel = async () => {
+    console.log("testing")
+  }
 
   const handleSave = async (e: SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -64,6 +73,10 @@ const Profile = () => {
     }
   }
 
+  const handleDelete = async () => {
+    console.log("testing")
+  }
+
   return (
     <>
       <main className={styles.mainCont}>
@@ -80,7 +93,10 @@ const Profile = () => {
           <div className='flex gap-1 justify-end'>
             {!edit ? (
               <>
-                <button type='button' className='-translate-y-0.25 hover:translate-none duration-200 cursor-pointer active:cursor-default rounded bg-red-500 text-white px-5 py-2'>Delete Account</button>
+                <button type='button' onClick={(e) => {
+                  e.preventDefault();
+                  setDelAcc(true);
+                }} className='-translate-y-0.25 hover:translate-none duration-200 cursor-pointer active:cursor-default rounded bg-red-500 text-white px-5 py-2'>Delete Account</button>
                 <button type='button' onClick={(e) => {
                   e.preventDefault();
                   setEdit(true)
@@ -99,6 +115,38 @@ const Profile = () => {
           </div>
         </form>
       </main>
+      <Dialog
+        open={delAcc}
+        onClose={() => setDelAcc(false)}
+        className="relative z-50"
+      >
+        <motion.div className="fixed inset-0 bg-black/60" 
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+        />
+        
+        <div className='fixed inset-0 flex w-screen items-center justify-center p-4'>
+            <DialogPanel className="mx-auto max-w-sm rounded bg-white p-5 max-sm:w-[90%] sm:w-[50%]">
+                <div className='flex justify-center flex-col items-center gap-3'>
+                    <TriangleAlert size={100} className='text-red-500'/>
+                    <div className='text-center'>
+                        <DialogTitle className="text-2xl font-bold">Are you sure?</DialogTitle>
+                        <p className="text-gray-500">Warning: This action cannot be undone.</p>
+                    </div>
+                    <div className='flex gap-3'>
+                        <button 
+                            type="button"
+                            className='px-5 py-2 bg-red-500 text-white rounded-full cursor-pointer hover:bg-red-600 duration-200 -translate-y-0.25 hover:translate-none shadow hover:shadow-none' 
+                            onClick={() => handleDelete()}>
+                                Yes
+                        </button>
+                        <button className='px-5 py-2 bg-gray-500 text-white rounded-full cursor-pointer hover:bg-gray-600 duration-200 -translate-y-0.25 hover:translate-none shadow hover:shadow-none' onClick={() => setDelAcc(false)}>Cancel</button>
+                    </div>
+                </div>
+            </DialogPanel>
+        </div>
+      </Dialog>
       <ToastContainer/>
     </>
   )
